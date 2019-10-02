4fefa8cefa148c789444b7f295a5943046858705
Tal Bar-Or <olsysystems@gmail.com>
2019-10-02 06:43:42 +0300

    forms

--- ITPortal/ittasks/views.py
+++ ITPortal/ittasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-#from .models import List
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from .models import MainTask, ChildTask 
 #from .forms import ListForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -8,4 +10,17 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
     return render(request ,'index.html', {})
+
+
+
+class HomePageView(ListView):
+
+    template_name = "index.html"
+    
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context = MainTask.objects.all()
+        return context
+
     
