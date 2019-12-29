1a3eb0e7422932ddef5fc6159aba249e0116cf8a
tbaror <tbaror@gmail.com>
2019-12-29 23:17:45 +0200

    form create update

--- ITPortal/ittasks/forms.py
+++ ITPortal/ittasks/forms.py
@@ -3,6 +3,16 @@ from .models import MainTask
 
 
 class TaskCraetionForm(forms.ModelForm):
+
+    TASK_STATUS_CHOICES = [
+    ('ST', 'STARTED'),
+    ('NS', 'NOT STARTED'),
+    ('IP', 'IN PROGRESS'),
+    ('PA', 'PAUSED'),
+    ('CO', 'COMPLETED'),
+    ]
+
+    
     task_title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Task Title'}))
     global_task_info = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Task Description'}))
     due_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={
@@ -10,7 +20,8 @@ class TaskCraetionForm(forms.ModelForm):
             'id': 'picker'
         }))
     #global_task_assign = forms.ChoiceField(widget=forms.ChoiceField(attrs={'class':'form-control'}))
-    
+    task_status = forms.ChoiceField(label='', choices=TASK_STATUS_CHOICES, widget=forms.Select(attrs={'class':'form-control'}))
+
     class Meta:
         model = MainTask
         fields = ['task_title',
@@ -18,5 +29,5 @@ class TaskCraetionForm(forms.ModelForm):
             'due_date',
             'global_task_assign',
             'task_status',
-        
+ 
         ]
