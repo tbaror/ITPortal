8c8381d8817e7cdfec20d0da25d3e53e5b7caca6
unknown <admin@WIN10NEW.dalet.local>
2020-01-27 18:56:09 +0200

    update form progress

--- ITPortal/ittasks/forms.py
+++ ITPortal/ittasks/forms.py
@@ -19,7 +19,8 @@ class TaskCraetionForm(forms.ModelForm):
 '%m/%d/%Y',
 '%Y/%m/%d',       # '10/25/2006'
 '%Y/%m/%d %H:%M',
-'%m/%d/%y']       # '10/25/06'
+'%m/%d/%y',
+'%Y-%m-%d %H:%M:%S']       # '10/25/06'
     
     task_title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Task Title'}))
     global_task_info = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Task Description'}))
@@ -29,6 +30,7 @@ class TaskCraetionForm(forms.ModelForm):
         }))
     global_task_assign = forms.ModelChoiceField(queryset= UserProfile.objects.all(), widget=forms.Select(attrs={'class':'form-control'} ))
     task_status = forms.ChoiceField(label='', choices=TASK_STATUS_CHOICES, widget=forms.Select(attrs={'class':'form-control'}))
+    task_location = forms.CharField(max_length=500, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Task Location'}))
 
     class Meta:
         model = MainTask
@@ -37,6 +39,7 @@ class TaskCraetionForm(forms.ModelForm):
             'due_date',
             'global_task_assign',
             'task_status',
+            'task_location',
  
         ]
 
@@ -55,12 +58,13 @@ class TaskUpdateForm(ModelForm):
 '%m/%d/%Y',
 '%Y/%m/%d',       # '10/25/2006'
 '%Y/%m/%d %H:%M',
-'%m/%d/%y']       # '10/25/06'
+'%m/%d/%y',
+'%Y-%m-%d %H:%M:%S']       # '10/25/06'
 
     #Main Task objects
-    task_title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Task Title'}))
-    global_task_info = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Task Description'}))
-    due_date = forms.DateTimeField(input_formats=INPUTֹTIMEֹFORMATS, widget=forms.DateTimeInput(attrs={
+    task_title = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Task Title'}))
+    global_task_info = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Task Description'}))
+    due_date = forms.DateTimeField(required=False, input_formats=INPUTֹTIMEֹFORMATS, widget=forms.DateTimeInput(attrs={
             'class': 'form-control',
             'id': 'picker'
         }))
@@ -68,7 +72,7 @@ class TaskUpdateForm(ModelForm):
     task_status = forms.ChoiceField(label='', choices=TASK_STATUS_CHOICES, widget=forms.Select(attrs={'class':'form-control'}))
     complete = forms.BooleanField( required=False, widget=forms.CheckboxInput(attrs={'type':'checkbox', 'class':'custom-control-input', 'id':'switchcomplete'}))
     overall_precent_complete = forms.IntegerField(widget=(forms.NumberInput(attrs={'type':'range', 'min':'1', 'max':'100', 'value':'50', 'class':'slider', 'id':'PreRange'})))
-    task_location = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Task Title'}))
+    task_location = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 
     #Child Tasks objects
     task_description = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Sub Task Description'}))
